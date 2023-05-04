3e62897e0d02781f100ed0dd0c87b2c13e091a06
Nathan <nslee333@gmail.com>
2023-05-04 08:57:15 -0700

    Added return for index in ChirpController.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,9 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return response('Hello, world!');
     }
 
     /**
